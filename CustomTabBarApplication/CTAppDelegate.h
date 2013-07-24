a18e52b2e2bd4114c1ac798838d8deb29da2c369
sajjankc <sajjan.rajan@gmail.com>
2013-07-24 21:14:58 +0545

    Initial Commit

---
+++ CustomTabBarApplication/CTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CTAppDelegate.h
+//  CustomTabBarApplication
+//
+//  Created by Sajjan on 7/24/13.
+//  Copyright (c) 2013 sajjankc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomTabBarApplication/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomTabBarApplication
+//
+//  Created by Sajjan on 7/24/13.
+//  Copyright (c) 2013 sajjankc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CTAppDelegate class]));
+    }
+}
